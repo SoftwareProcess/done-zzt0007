980572613c54638dab51729663579a55fa2e0364
zzt0007 <89746313+zzt0007@users.noreply.github.com>
2021-08-29 18:01:53 -0500

    Update collapse.py

--- Assignment/collapse.py
+++ Assignment/collapse.py
@@ -1,2 +1,14 @@
 def collapse(value):
-    ...
+    digits = len(value)
+    if value.isnumeric() and digits < 51:
+        while digits == 1:
+            return value
+        while digits > 1:
+            number = 0
+            for values in value:
+                number += int(values)
+            value = str(number)
+            digits = len(str(number))
+        return value
+    else: 
+        return None 
