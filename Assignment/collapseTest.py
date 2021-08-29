ad03b043df188ee99a9a2590cc330f1542defd67
zzt0007 <89746313+zzt0007@users.noreply.github.com>
2021-08-29 18:02:18 -0500

    Update collapseTest.py

--- Assignment/collapseTest.py
+++ Assignment/collapseTest.py
@@ -11,3 +11,53 @@ class CollapseTest(unittest.TestCase):
         actualResult = collapse.collapse(value)
         self.assertEqual(expectedResult, actualResult)
     
+    
+# Happy path test
+    def test_ShouldCollapseSingleDigit(self):
+        value = '0'
+        expectedResult = '0'
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+    
+    
+# Happy path test
+    def test_ShouldCollapseAllDigit(self):
+        value = '98769'
+        expectedResult = '3'
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+    
+    
+
+# Happy path test
+    def test_ShouldCollapseAllDigits(self):
+        value = '123'
+        expectedResult = '6'
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+    
+    
+# Happy path test
+    def test_ShouldNotCollapseLetters(self):
+        value = 'abc'
+        expectedResult = None 
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+    
+    
+# Happy path test
+    def test_ShouldNotCollapseNoDigit(self):
+        value = ''
+        expectedResult = None
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+    
+    
+# Happy path test
+    def test_ShouldNotCollapseOverFiftyDigits(self):
+        value = '123456789012345678901234567890123456789012345678901'
+        expectedResult = None 
+        actualResult = collapse.collapse(value)
+        self.assertEqual(expectedResult, actualResult)
+    
+    
